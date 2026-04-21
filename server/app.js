d618b12056321a623b4bba10d49b1ac931bd074c
tova d <itta0527639865@gmail.com>
2026-04-21 19:29:04 +0300

    Created basic ClassroomList component and connected server with CORS

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
+const cors = require('cors'); // למעלה עם ה-require
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(cors()); 
 app.get('/', (req, res) => {
   res.send('Server is running!');
+});
+//רק לצורך בדיקה
+//רשימה דמיונית שאחרי זה נשים במסד
+app.get('/api/classrooms', (req, res) => {
+  const classrooms = [
+    { _id: '1', name: 'כיתה א1', floor: '1', capacity: 30 },
+    { _id: '2', name: 'מעבדת מחשבים', floor: '2', capacity: 20 },
+    { _id: '3', name: 'אודיטוריום', floor: '0', capacity: 100 }
+  ];
+  res.json(classrooms);
 });
 
 app.listen(port, () => {
