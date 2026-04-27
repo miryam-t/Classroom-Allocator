48bf515d0e49bfbcfcbd39adf464cd6486a28c6d
tova d <itta0527639865@gmail.com>
2026-04-27 15:35:28 +0300

    Add MUI Bottom Navigation with colorful app-style buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import  ClassroomList  from "./components/ClassroomList";
-
+import Navbar from "./components/Navbar";
 function APP(){
   return(
     <div>
+      <Navbar></Navbar>
       <h1>מערכת הקצאת כיתות</h1>
       <ClassroomList></ClassroomList>
     </div>
