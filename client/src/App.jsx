ea4646dc7ad009d13da1a23bd479c8e807200d3f
tova d <itta0527639865@gmail.com>
2026-04-29 21:05:41 +0300

    Temporary line for testing only

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import  ClassroomList  from "./components/ClassroomList";
 import Navbar from "./components/Navbar";
+import AddCancellation from "./components/AddCancellation";
 function APP(){
   return(
     <div>
       <Navbar></Navbar>
       <h1>מערכת הקצאת כיתות</h1>
       <ClassroomList></ClassroomList>
+      {/* 2. תוסיפי את השורה הזו באופן זמני: */}
+      {/* <AddCancellation 
+        classroomId="123" 
+        classroomName="חדר בדיקה 101" 
+        onClose={() => console.log('החלון נסגר')} 
+      /> */}
     </div>
   )
 }
