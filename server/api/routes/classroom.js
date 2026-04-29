109bb5683f1c5d039a3d479326e207fab73c1892
tova d <itta0527639865@gmail.com>
2026-04-29 19:39:45 +0300

    SERVER: Add route for smart cancellation

--- server/api/routes/classroom.js
+++ server/api/routes/classroom.js
@@ -1,11 +1,17 @@
 import express from 'express';
 const router = express.Router();
 // ייבוא הפונקציות מהקונטרולר - חובה לציין סיומת .js
-import { getById, createClassroom, updateClassroom, deleteClassroom } from '../controller/Classroom.js';
+// שינוי: הוספתי את addCancellation לרשימת הייבוא כדי שהראוטר יכיר את הפונקציה שלך
+import { getById, createClassroom, updateClassroom, deleteClassroom, addCancellation } from '../controller/Classroom.js';
 
 router.get('/classroom/:id', getById);
 router.post('/classroom', createClassroom); 
 router.put('/classroom/:id', updateClassroom);
 router.delete('/classroom/:id', deleteClassroom);
+
+// ---  הוספת נתיב לביטול חדר (SERVER) ---
+// שינוי על פי הוראות התיכון החדשות: "בקשת עדכון במקום מחיקה"
+// הכתובת הזו מאפשרת למזכירה לשלוח בקשת ביטול לחדר ספציפי לפי ה-ID שלו
+router.patch('/classroom/:id/cancel', addCancellation); 
 
 export default router;
