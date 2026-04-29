b662d4483fd8b247e6ef8cc8cc2ec434fa50d11c
tova d <itta0527639865@gmail.com>
2026-04-29 17:57:27 +0300

    Update Classroom schema to include startTime and endTime in cancellations

--- server/api/models/Classroom.js
+++ server/api/models/Classroom.js
@@ -8,6 +8,7 @@ const classroomSchema = new mongoose.Schema({
     capacity: { type: Number, required: true },
     hasProjector: { type: Boolean, default: false },
     
+    // מערך השיבוצים - באחריות ציפורה
     allocations: [{ 
         day: String, 
         startTime: String, 
@@ -15,9 +16,22 @@ const classroomSchema = new mongoose.Schema({
         courseName: String 
     }],
     
+    // מערך הביטולים -משימה 3 בדאטה - ציפורה - כאן השינוי שלך בא לידי ביטוי (DATA)
     cancellations: [{ 
-        date: Date, 
-        reason: String 
+        // ...תאריך הביטול חייבים למלא כי לדעת שעות בלי תאריך זה חסר משמעות
+        date: { type: Date, required: true }, 
+        
+        // --- שינוי: הוספת שדות זמן ---
+        //השדות האלו קריטיים כדי שהמערכת תדע "לשחרר" את החדר בשעות ספציפיות
+        //ולא רק לפי תאריך
+        startTime: { type: String, required: true }, // למשל: "08:50"
+        endTime: { type: String, required: true },   // למשל: "09:35"
+        
+        reason: { type: String }, // סיבת הביטול (אופציונלי)
+        
+        // --- בונוס: קישור לשיבוץ המקורי ---
+        // עוזר למערכת לדעת איזה קורס בוטל בתוך הבלוק הזה
+        originalCourseName: String 
     }]
 });
 
