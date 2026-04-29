fdf569703b6915ebe1ea1479a14e73d7c656c9ea
Miryam T <miryamt05271@gmail.com>
2026-04-29 12:39:41 +0300

    כתבתי את משימת ניהול חדרים מי שצריכה לכתוב את פונקיות מחיקת חדר ומחיקת כל השיבוצים צריכה להמשיך אותי הכנתי את כל הבסיס ,כל מה שצריך בצד לקוח כבר כתוב

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import  ClassroomList  from "./components/ClassroomList";
+import RoomManagement from "./components/RoomManagement";
 
 function APP(){
   return(
     <div>
       <h1>מערכת הקצאת כיתות</h1>
       <ClassroomList></ClassroomList>
+      <RoomManagement></RoomManagement>
     </div>
   )
 }
