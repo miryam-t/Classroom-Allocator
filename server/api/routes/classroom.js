5f7ad5cfc5e57613d18a6039404e0df728660d76
Dina Fridman <dina624048Qgmail.com>
2026-05-11 14:08:28 +0300

    Fixed SyntaxError in classroom routes and added basic Allocation CRUD infrastructure

--- server/api/routes/classroom.js
+++ server/api/routes/classroom.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 // ייבוא הפונקציות מהקונטרולר - חובה לציין סיומת .js
-import { getById, createClassroom, updateClassroom, deleteClassroom } from '../controller/Classroom.js';
+// import { getById, createClassroom, updateClassroom, deleteClassroom } from '../controller/Classroom.js';
 import {searchAvailableClassrooms} from '../controller/searchClassroom.js'
 //ייבוא המידלוואר
 import { validateRoomParams } from '../middleware/validateRoom.js';
